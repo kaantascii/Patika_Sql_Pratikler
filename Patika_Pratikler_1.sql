5fd63ce5f305e075664d5a8b82ba7a7158d6a5e0
Kaan Taşcı <k_mert@windowslive.com>
2025-06-25 15:37:12 +0300

    commit

---
+++ Patika_Pratikler_1.sql
@@ -0,0 +1,23 @@
+/*SELECT title,description from film; 
+*/
+
+/*SELECT *
+FROM film
+WHERE length > 60 AND length < 75
+ORDER BY length;
+*/
+/*
+SELECT *
+FROM film
+WHERE rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99;  
+*/
+
+/*
+SELECT * FROM customer
+WHERE first_name ILIKE = 'Mary' ;
+*/
+
+/*
+SELECT * FROM Film 
+WHERE (length <50 and rental_rate = '2.99') or  rental_rate != '4.99';
+*/
